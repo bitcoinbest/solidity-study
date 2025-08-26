1698174a56b80b2e9325abf3f4b9ca9cb1c4c814
dan <dans88456@gmail.com>
2025-08-26 16:17:17 +0800

    update greeting again

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract MyContract {
-    string public greeting = "Hello, blockchain!";
+    string public greeting = "Hello, blockchain! agin";
 
     function sayHello() public view returns (string memory) {
         return greeting;
